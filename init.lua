a86529ef0e0e8d53266386313b150259f61d337b
gabriel1379 <gabriel1379@gmail.com>
2025-02-09 16:03:02 +0100

    Fix unsaved files

--- init.lua
+++ init.lua
@@ -4,7 +4,12 @@
 
 -- This mod enables seamless integration between Quarry Mechanics and other mods' resource and stone-like nodes.
 
-local path_own = minetest.get_modpath("quarry_link")
+local path_own = minetest.get_modpath('quarry_link')
+local mods_linked = {
+    'everness',
+    'technic',
+}
 
-dofile(path_own.."/link_everness.lua")
-dofile(path_own.."/link_technic.lua")
+for _,mod_linked in pairs(mods_linked) do
+    dofile(path_own..'/link_'..mod_linked..'.lua')
+end
