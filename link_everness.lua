55b48cf809c168d569e0ed8310ba513b54555c0f
gabriel1379 <gabriel1379@gmail.com>
2025-02-28 21:35:15 +0100

    Remove old, unused code

--- link_everness.lua
+++ link_everness.lua
@@ -134,20 +134,3 @@ local irregularly_named_pairs = {
 }
 
 quarry_link.link(mod_name, conversions_by_tool, irregularly_named_pairs)
-
--- cursed_stone_carved
--- quartz_block
-
---[[ local stones_with_block_variant_everness = {
-    "Coral Desert Stone",
-    "Mineral Stone",
-}
-
-for _,stone_name in ipairs(stones_with_block_variant_everness) do
-    quarry_link.register_cut_stone_or_block(stone_name, mod_name)
-    quarry_link.register_cut_stone_or_block(stone_name.." Block", mod_name)
-    quarry_link.set_tools_for_stone(stone_name, mod_name, false, false)
-    quarry_link.set_tools_for_stair_and_slab(stone_name, mod_name, false, conversions_by_tool)
-    quarry_link.clear_crafts(stone_name, mod_name)
-    quarry_link.register_block_craft_recipe(stone_name)
-end ]]
